975da5daabcce475d73e7d0747b2fe4bf9d55ecb
BragaSoterasVinicius <viniciussoteras@gmail.com>
2023-06-07 13:34:33 -0300

    Tentando solucionar o bug anterior
    Talvez eu deveria colocar o EventListener no começo do código, afinal a falha parece vir de que este não detecta a função que está atrás de si. (n vo nem falar que javascript e uma linguagem assíncrona e que esqueci uns pontos e vírgulas)

--- js/inds.js
+++ js/inds.js
@@ -1,18 +1,18 @@
 const intro = document.querySelector(".intro");
 var ace=0
 var a = document.getElementsByClassName("intro")
-    document.addEventListener('DOMContentLoaded', (e)=>{
 
-        setTimeout(()=>{intro.classList.add('display-none');ace = 1;}, 2000);
-    })
-
-window.transitionToPage = function(href) {
-    document.querySelector('body').style.opacity = 0
-    setTimeout(function() { 
-        window.location.href = href
-    }, 500)
-}
+document.addEventListener('DOMContentLoaded', (e)=>{
+    setTimeout(()=>{intro.classList.add('display-none');ace = 1;}, 2000);
+})
 
 document.addEventListener('DOMContentLoaded', function(event) {
     document.querySelector('body').style.opacity = 100
-})
+});
+    
+window.transitionToPage = function(href) {
+    document.querySelector('body').style.opacity = 0;
+    setTimeout(function() { 
+        window.location.href = href
+    }, 500);
+}
